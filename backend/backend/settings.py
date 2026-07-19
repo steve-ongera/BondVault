3a95fa05a1bad7d35ad74a86add00b838fdce48e
steve-ongera <gadafisteve001@gmail.com>
2026-07-19 21:17:23 +0300

    update

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -191,4 +191,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+ 
+# ----------------------------------------------------------------------
+# Media (KYC documents, statements, tax files)
+# ----------------------------------------------------------------------
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "media"
